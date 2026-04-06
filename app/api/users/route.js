8793b45e6f02094f4a9a4ad4d3c2bc36c0bdfb07
Jeet Paul <jeetpaul2222@gmail.com>
2026-04-06 20:10:28 +0530

    add get end point

---
+++ app/api/users/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+let users = [
+  { id: 1, name: "Jeet Paul", email: "jeet@gmail.com" },
+  { id: 2, name: "Saheli Paul", email: "saheli@gmail.com" },
+  { id: 3, name: "Saikat Ghosh", email: "saikat@gmail.com" },
+];
+
+export const GET = async () => {
+  try {
+    return NextResponse.json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to fetch users",
+      },
+      { status: 500 },
+    );
+  }
+};
